161bea0b9215bf068be653f4a5e50238dbb5b961
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-04-18 18:33:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,55 +2,8 @@
 # Course Instructor :  Prof. Jaspreet Kaur Dhanjal
 
 
-protein_sequence = "WHGCITVYWMTV"
+protein_sequence = "SGFRKMAFPSGKVEGCMVQVTCGTTTLNGLWLDDTVYCPRHVICTAEDMLNPNYEDLLIRKSNHSFLVQAGNVQLRVIGHSMQNCLLRLKVDTSNPKTPKYKFVRIQPGQTFSVLACYNGSPSGVYQCAMRPNHTIKGSFLNGSCGSVGF"
 
-# alpha_propensity = {
-#     "G" : 0.53,
-#     "A" : 1.45,
-#     "V" : 1.14,
-#     "L" : 1.34,
-#     "I" : 1.00,
-#     "R" : 0.79,
-#     "K" : 1.07,
-#     "E" : 1.53,
-#     "D" : 0.98,
-#     "Q" : 1.17,
-#     "N" : 0.73,
-#     "T" : 0.82,
-#     "S" : 0.79,
-#     "C" : 0.77,
-#     "M" : 1.20,
-#     "F" : 1.12,
-#     "Y" : 0.61,
-#     "W" : 1.14,
-#     "H" : 1.24,
-#     "P" : 0.59
-# }
-    
-# beta_propensity = {
-#     "G" : 0.81,
-#     "A" : 0.97,
-#     "V" : 1.65,
-#     "L" : 1.22,
-#     "I" : 1.60,
-#     "R" : 0.90,
-#     "K" : 0.74,
-#     "E" : 0.26,
-#     "D" : 0.80,
-#     "Q" : 1.23,
-#     "N" : 0.65,
-#     "T" : 1.20,
-#     "S" : 0.72,
-#     "C" : 1.30,
-#     "M" : 1.67,
-#     "F" : 1.28,
-#     "Y" : 1.29,
-#     "W" : 1.19,
-#     "H" : 0.71,
-#     "P" : 0.62
-# }
-
-##########################################################   delete whats between
 alpha_propensity = {
     "G" : 0.53,
     "A" : 1.45,
@@ -89,13 +42,60 @@ beta_propensity = {
     "T" : 1.20,
     "S" : 0.72,
     "C" : 1.30,
-    "M" : 1.17,
+    "M" : 1.67,
     "F" : 1.28,
     "Y" : 1.29,
     "W" : 1.19,
     "H" : 0.71,
     "P" : 0.62
 }
+
+##########################################################   delete whats between
+# alpha_propensity = {
+#     "G" : 0.53,
+#     "A" : 1.45,
+#     "V" : 1.14,
+#     "L" : 1.34,
+#     "I" : 1.00,
+#     "R" : 0.79,
+#     "K" : 1.07,
+#     "E" : 1.53,
+#     "D" : 0.98,
+#     "Q" : 1.17,
+#     "N" : 0.73,
+#     "T" : 0.82,
+#     "S" : 0.79,
+#     "C" : 0.77,
+#     "M" : 1.20,
+#     "F" : 1.12,
+#     "Y" : 0.61,
+#     "W" : 1.14,
+#     "H" : 1.24,
+#     "P" : 0.59
+# }
+    
+# beta_propensity = {
+#     "G" : 0.81,
+#     "A" : 0.97,
+#     "V" : 1.65,
+#     "L" : 1.22,
+#     "I" : 1.60,
+#     "R" : 0.90,
+#     "K" : 0.74,
+#     "E" : 0.26,
+#     "D" : 0.80,
+#     "Q" : 1.23,
+#     "N" : 0.65,
+#     "T" : 1.20,
+#     "S" : 0.72,
+#     "C" : 1.30,
+#     "M" : 1.17,
+#     "F" : 1.28,
+#     "Y" : 1.29,
+#     "W" : 1.19,
+#     "H" : 0.71,
+#     "P" : 0.62
+# }
 ##########################################################
 
 
@@ -201,6 +201,8 @@ if __name__ == '__main__':
             prediction[i] = "S"
             i += 1
         
+        elif found_alphas[i] == " " and found_betas[i] == " ":
+            i += 1
         else:
             upto = i
             while upto < len(prediction) and found_alphas[upto] == "H" and found_betas[upto] == "S":
@@ -222,6 +224,6 @@ if __name__ == '__main__':
             i = upto + 1
 
 
-    print("Structure Prediction as Per Chou-Fasman :  ", end = "")
+    print("Structure Prediction as Per Chou-Fasman : ")
     for e in prediction:
         print(e , end = "")
